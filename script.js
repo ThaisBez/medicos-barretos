3af9d8e1f1f0e0f9e9adcec69c72317662d57044
ThaisBez <133007394+ThaisBez@users.noreply.github.com>
2026-06-22 09:28:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+fetch("dadosmedico.csv")
+  .then(resposta => resposta.text())
+  .then(texto => {
+    console.log(texto);
+  })
+  .catch(erro => {
+    console.error("Erro ao carregar o CSV:", erro);
+  });
